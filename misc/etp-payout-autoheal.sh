60c36437fb1be3a1c15b6074034eb3876dcb3b1c
Craig Crawford <craigcrawford1988@gmail.com>
2018-09-26 21:22:38 +0100

    Silly indentation

--- misc/etp-payout-autoheal.sh
+++ misc/etp-payout-autoheal.sh
@@ -5,9 +5,9 @@ SERVICE="oep-etp-payouts"
 
 systemctl -n 1 status $SERVICE | grep -i "payments suspended"
 if [ $? -eq 0 ]; then
-	grep -i "(empty list or set)" <<< $(redis-cli --no-raw ZREVRANGE "$PAYMENTS:pending" 0 -1 WITHSCORES)
-	if [ $? -eq 0 ]; then
-		redis-cli DEL "$PAYMENTS:lock"
-		systemctl restart $SERVICE
-	fi
+    grep -i "(empty list or set)" <<< $(redis-cli --no-raw ZREVRANGE "$PAYMENTS:pending" 0 -1 WITHSCORES)
+    if [ $? -eq 0 ]; then
+        redis-cli DEL "$PAYMENTS:lock"
+        systemctl restart $SERVICE
+    fi
 fi
